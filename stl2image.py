8859579c071bb0d1a050dabec60b2cac688c7ac6
Anurag Uniyal <auniyal@vmware.com>
2015-06-07 15:49:44 -0700

    Removed pre-exising cube, only rotate on one axis, by default at 45, but can be overridden by OBJ_ROOT env var

--- stl2image.py
+++ stl2image.py
@@ -2,36 +2,36 @@
 stl2image script takes a stl file and renders it into a image file e.g.
 /Applications/Blender/blender.app/Contents/MacOS/blender --python stl2image.py -- ~/Downloads/bearing.stl /tmp/b8.png
 """
-
+import os
 import sys
 import math
 
 import bpy
 import mathutils
 
-def set_camera_lights(target):
+def set_camera_lights(target, obj_rot):
     # set the origin to bounding box center
     bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='BOUNDS')
     # move camera away from target we want to shoot buggest side so we should move away in direction of shortest side
     min_d = min(target.dimensions)
     # move camera by double of max dimension
-    cam_move = max(target.dimensions)*1.5
-    obj_rot = math.radians(20) # how much to rotate object
+    cam_move = max(target.dimensions)*1.4
+    obj_rot = math.radians(obj_rot) # how much to rotate object
     if target.dimensions.x == min_d:
         cam_location = (cam_move, 0, 0)
         lamp_location = (cam_move*1.5, 0, 0)
         cam_rotation = (0.0, math.radians(90), 0.0)
-        target_rotation = (0.0, obj_rot, obj_rot)
+        target_rotation = (0.0, 0.0, obj_rot)
     elif target.dimensions.y == min_d:
         cam_location = (0, cam_move, 0)
         lamp_location = (0, cam_move*1.5, 0)
         cam_rotation = (-math.radians(90), 0.0, 0.0)
-        target_rotation = (obj_rot, 0.0, obj_rot)
+        target_rotation = (0.0, 0.0, obj_rot)
     else:
         cam_location = (0, 0, cam_move)
         lamp_location = (0, 0, cam_move*1.5)
         cam_rotation = (0.0, 0.0, 0.0)
-        target_rotation = (obj_rot, obj_rot, 0.0)
+        target_rotation = (obj_rot, 0.0, 0.0)
 
     # rotate object a bit
     target.rotation_euler = target_rotation
@@ -54,6 +54,15 @@ def import_stl(stlfile):
     """
     import stl file and return the object
     """
+    # before importing lets delete Cube generally found in blender default projects
+    try:
+        bpy.data.objects['Cube'].select = True
+    except KeyError as e:
+        print("Cube not found.")
+    else:
+        bpy.ops.object.delete()
+        print("Deleted cube.")
+
     bpy.ops.import_mesh.stl(filepath=stlfile)
     # blender will import object with name=filename but capitalized
     # we can rely on that or for now imported object should be active
@@ -68,11 +77,12 @@ def save_image(imagefile):
     bpy.data.scenes['Scene'].render.filepath = imagefile
     bpy.ops.render.render( write_still=True )
 
+obj_rot = int(os.environ.get('OBJ_ROT', 45))
 stlfile = sys.argv[-2]
 imagefile = sys.argv[-1]
 
 target = import_stl(stlfile)
-set_camera_lights(target)
+set_camera_lights(target, obj_rot)
 save_image(imagefile)
 
 #bpy.ops.wm.quit_blender()
